af9379f09ff608a4946e7de743e749031dac7990
Ammar Marvi <ammar.marvi01@gmail.com>
2025-10-05 03:14:44 -0400

    svm

--- svm.py
+++ svm.py
@@ -3,27 +3,56 @@ import pickle
 import numpy as np
 import pandas as pd
 from PIL import Image
-from sklearn import svm
+from sklearn import svm, preprocessing, model_selection, metrics, pipeline
 from tqdm import tqdm
 
-'''
-TODO:
+def stack_features(features, layer):
+    num_reps = len(features[ids[0]])  # 100
+    X, y, groups = [], [], []
+    for rep in range(num_reps):
+        for cls_id, cls_name in enumerate(ids):
+            vec = np.asarray(features[cls_name][rep][layer]).ravel()
+            X.append(vec)
+            y.append(cls_id)
+            groups.append(rep)  # group = triplet id
+    return np.vstack(X), np.array(y), np.array(groups)
 
-finish function for running svm
-decide on id: 
-    same_img vs same_label vs diff_label?
-    one vs two?
-    none vs one vs two?
-'''
-# set up svm
-num_ids = 3
-num_reps = 100
-num_samples = num_reps * num_ids
+fname = os.path.join('features', 'mnist_features.pkl')
+with open(fname, 'rb') as handle:
+    features = pickle.load(handle)
+    
+ids = list(features.keys())
 
-indTest = np.arange(0,num_samples,num_reps)
-indAll = np.arange(0,num_samples)
+layer = "fc7"
 
+act, trainCat, groups = stack_features(features, layer)
+num_reps = len(np.unique(groups))
+num_ids = len(ids)
+num_samples = act.shape[0]
+
+clf = pipeline.make_pipeline(
+    preprocessing.StandardScaler(),
+    svm.LinearSVC(dual='auto', class_weight='balanced', max_iter=5000, random_state=0)
+)
+
+logo = model_selection.LeaveOneGroupOut()
 perf_fold = np.zeros(shape=(num_reps,))
+y_true_all, y_pred_all = [], []
 
-x = np.arange(0,num_ids)
-trainCat = np.repeat(x,num_reps-1)
+for fold_idx, (indTrain, indTest) in tqdm(enumerate(logo.split(act, trainCat, groups))):
+    clf.fit(act[indTrain, :], trainCat[indTrain])
+    dec = clf.predict(act[indTest, :])
+
+    perf_fold[fold_idx] = metrics.accuracy_score(trainCat[indTest], dec)
+    y_true_all.extend(trainCat[indTest])
+    y_pred_all.extend(dec)
+
+overall_acc = perf_fold.mean()
+cm = metrics.confusion_matrix(y_true_all, y_pred_all, labels=[0,1,2])
+
+print(f"Overall accuracy: {overall_acc:.3f}")
+print("Per-fold accuracy (one triplet per fold):", perf_fold.shape, "folds")
+print("Confusion matrix (rows=true, cols=pred):")
+print(cm)
+print("Label order:", class_names)
+
